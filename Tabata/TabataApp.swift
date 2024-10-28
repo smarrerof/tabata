bc0f7b4e98ef52918e5f5d223e299e285b89ff8b
Sergio Marrero Fernandez <sergio@Sergios-Mac-mini.local>
2024-10-28 18:48:57 +0000

    Initial commit

---
+++ Tabata/TabataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataApp.swift
+//  Tabata
+//
+//  Created by Sergio Marrero Fernandez on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TabataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
