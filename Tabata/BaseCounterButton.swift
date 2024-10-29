5a438bcc1448f5c1cc3c12d279c01042b0c29dd5
Sergio Marrero Fernandez <sergio@Sergios-Mac-mini.local>
2024-10-29 20:02:41 +0000

    Created base counter button

---
+++ Tabata/BaseCounterButton.swift
@@ -0,0 +1,18 @@
+//
+//  BaseCounterButton.swift
+//  Tabata
+//
+//  Created by Sergio Marrero Fernandez on 29/10/24.
+//
+
+import SwiftUI
+
+struct BaseCounterButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaseCounterButton()
+}
